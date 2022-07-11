72a0a4c569b1348ef9d8e9fcf830d1bd343a5178
Adilene Luna <lunaadilene65@gmail.com>
2022-07-11 15:24:00 -0500

    login registro

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,4 +1,4 @@
-// If we need to use custom DOM library, let's save it to $$ variable:
+/// If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
 var app = new Framework7({
@@ -14,10 +14,9 @@ var app = new Framework7({
     },
     // Add default routes
     routes: [
-      {
-        path: '/about/',
-        url: 'about.html',
-      },
+      { path: '/about/', url: 'about.html',},
+      { path: '/inicio-sesion/', url: 'inicio-sesion.html',},
+      { path: '/registrarse/', url: 'registrarse.html',},
     ]
     // ... other parameters
   });
@@ -27,7 +26,16 @@ var mainView = app.views.create('.view-main');
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
+   
+
+  
+     
+
 });
+
+
+
+
 
 // Option 1. Using one 'page:init' handler for all pages
 $$(document).on('page:init', function (e) {
@@ -36,8 +44,35 @@ $$(document).on('page:init', function (e) {
 })
 
 // Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="about"]', function (e) {
+$$(document).on('page:init', '.page[data-name="registrarse"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
+    $$('#bregistro').on('click', fnRegistro );
+    
+    
 })
+
+function fnRegistro(){
+//alert('entro');
+  var email = $$('#rEmail').val();
+  var password = $$('#rPassword').val();
+
+  console.log(email);
+  console.log(password);
+
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+    console.log('usuario creado');
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  console.log(errorCode + "--" + errorMessage);
+
+    // ..
+  });
+
+}
+
