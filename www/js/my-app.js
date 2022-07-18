6616ec11576999adf84805f827bb4598b834f2a9
Adilene Luna <lunaadilene65@gmail.com>
2022-07-18 14:37:08 -0500

    Se agrega html para registro productos categorias

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -2,77 +2,198 @@
 var $$ = Dom7;
 
 var app = new Framework7({
-    // App root element
-    root: '#app',
-    // App Name
-    name: 'My App',
-    // App id
-    id: 'com.myapp.test',
-    // Enable swipe panel
-    panel: {
-      swipe: 'left',
-    },
-    // Add default routes
-    routes: [
-      { path: '/about/', url: 'about.html',},
-      { path: '/inicio-sesion/', url: 'inicio-sesion.html',},
-      { path: '/registrarse/', url: 'registrarse.html',},
-    ]
-    // ... other parameters
-  });
+  // App root element
+  root: '#app',
+  // App Name
+  name: 'My App',
+  // App id
+  id: 'com.myapp.test',
+  // Enable swipe panel
+  panel: {
+    swipe: 'left',
+  },
+  // Add default routes
+  routes: [
+    { path: '/about/', url: 'about.html', },
+    { path: '/inicio-sesion/', url: 'inicio-sesion.html', },
+    { path: '/registrarse/', url: 'registrarse.html', },
+    { path: '/index/', url: 'index.html', },
+    { path: '/categorias/', url: 'categorias.html', },
+    { path: '/productos/', url: 'productos.html', },
+  ]
+  // ... other parameters
+});
 
 var mainView = app.views.create('.view-main');
 
 // Handle Cordova Device Ready Event
-$$(document).on('deviceready', function() {
-    console.log("Device is ready!");
-   
+$$(document).on('deviceready', function () {
+  console.log("Device is ready!");
+  //sembrar();
+  //agregarProducto();
 
-  
-     
+})
 
-});
+function sembrar() {
+  var db = firebase.firestore();
+  var data = {
 
+    nombre: "leonardo",
+    mail: "pepe@hotmail.com",
+    rol: "developer"
 
+  };
+  db.collection("personas").add(data)
+    .then(function (docRef) { // .then((docRef) => {
+      console.log("OK! Con el ID: " + docRef.id);
+    })
+    .catch(function (error) { // .catch((error) => {
+      console.log("Error: " + error);
+    })
+}
 
+function agregarProducto() {
+  var db = firebase.firestore();
 
+  var producto = {
+
+    nombre: "blusa",
+    precio: "$180",
+    color: "blanco",
+
+  };
+  db.collection("productos").add(producto)
+    .then(function (docRef) { // .then((docRef) => {
+      console.log("OK! Con el ID: " + docRef.id);
+    })
+    .catch(function (error) { // .catch((error) => {
+      console.log("Error: " + error);
+    })
+}
 
 // Option 1. Using one 'page:init' handler for all pages
 $$(document).on('page:init', function (e) {
-    // Do something here when page loaded and initialized
-    console.log(e);
+  // Do something here when page loaded and initialized
+  console.log(e);
 })
 
 // Option 2. Using live 'page:init' event handlers for each page
 $$(document).on('page:init', '.page[data-name="registrarse"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    $$('#bregistro').on('click', fnRegistro );
-    
-    
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  $$('#bregistro').on('click', fnRegistro);
 })
 
-function fnRegistro(){
-//alert('entro');
+$$(document).on('page:init', '.page[data-name="index"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+})
+
+
+$$(document).on('page:init', '.page[data-name="inicio-sesion"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  $$('#bingresa').on('click', bingresa);
+})
+$$(document).on('page:init', '.page[data-name="categorias"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  $$('#btnAgregarCategoria').on('click', agregaCategoria);
+  
+})
+
+$$(document).on('page:init', '.page[data-name="productos"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  $$('#agregarTProducto').on('click', agregarProducto);
+  
+})
+
+
+function bingresa() {
+
+  var email = $$('#mail').val();
+  console.log(email);
+  var password = $$('#password').val();
+  console.log(password);
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+}
+
+
+function fnRegistro() {
+  //alert('entro');
   var email = $$('#rEmail').val();
   var password = $$('#rPassword').val();
 
   console.log(email);
   console.log(password);
 
-firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    // ...
-    console.log('usuario creado');
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  console.log(errorCode + "--" + errorMessage);
-
-    // ..
-  });
-
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+      console.log('usuario creado');
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode + "--" + errorMessage);
+      // ..
+    });
 }
 
+function Regresar() {
+  //window.location.href = "/index/";
+}
+
+function agregaCategoria() {
+  var nombreCategoria = $$("#acategorias").val();
+  var db = firebase.firestore();
+  var categoria = {
+    nombre: nombreCategoria
+  };
+  db.collection("categorias").add(categoria)
+    .then(function (docRef) { // .then((docRef) => {
+      console.log("OK! Con el ID: " + docRef.id);
+    })
+    .catch(function (error) { // .catch((error) => {
+      console.log("Error: " + error);
+    })
+  console.log('Agregando categorias');
+}
+
+function agregarProducto() {
+  console.log("inicio agregar producto");
+  var nombreProducto = $$('#nombreProducto').val();
+  var colorProducto = $$('#colorProducto').val();
+  var precioProducto = $$('#precioProducto').val();
+  var imagenProducto = $$('#imagenProducto').val();
+  console.log('Datos');
+  console.log(nombreProducto);
+  console.log(colorProducto);
+  console.log(precioProducto);
+  console.log(imagenProducto);
+
+  var db = firebase.firestore();
+  var producto = {
+    color: colorProducto,
+    nombre: nombreProducto,
+    precio: precioProducto,
+    imagen: imagenProducto
+  }
+  db.collection("MisProductos").add(producto)
+  .then(function (docRef) { // .then((docRef) => {
+    console.log("OK! Con el ID: " + docRef.id);
+  })
+  .catch(function (error) { // .catch((error) => {
+    console.log("Error: " + error);
+  })
+console.log('Agregando categorias');
+
+}
